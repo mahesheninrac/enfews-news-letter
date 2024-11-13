8a1cf9af4b2d14a8767f84c717d4d2f7af5c915e
mahesheninrac <mahesh.solanke@eninrac.com>
2024-11-13 10:04:01 +0530

    updated oct week 4 newsletter

--- script.js
+++ script.js
@@ -1,156 +1,32 @@
-// Script.js
-
-// Select the elements
-const video = document.getElementById("video");
-const videoThumbnail = document.getElementById("video-thumbnail");
-const playpause = document.getElementById("play-pause");
-const frwd = document.getElementById("skip-10");
-const bkwrd = document.getElementById("skipminus-10");
-const volume = document.getElementById("volume");
-const mutebtn = document.getElementById("mute");
-const videoContainer = document.querySelector(".video-container");
-const controls = document.querySelector(".controls");
-const progressBar = document.querySelector(".progress-bar");
-const playbackline = document.querySelector(".playback-line");
-const currentTimeRef = document.getElementById("current-time");
-const maxDuration = document.getElementById("max-duration");
-
-const timeFormatter = (timeInput) => {
-    let minute = Math.floor(timeInput / 60);
-    minute = minute < 10 ? "0" + minute : minute;
-    let second = Math.floor(timeInput % 60);
-    second = second < 10 ? "0" + second : second;
-    return `${minute}:${second}`;
-};
-
-// Play-Pause
-playpause.addEventListener("click", function () {
-    if (video.paused) {
-        videoThumbnail.style.display = "none";
-        video.play();
-        playpause.innerHTML = '<i class="fa-solid fa-pause"></i>';
-    } else {
-        video.pause();
-        playpause.innerHTML = '<i class="fa-solid fa-play"></i>';
+function pow(a, b) {
+    let result = 1;
+    for (let i = 0; i < b; i++) {
+        result *= a;
     }
-});
-
-let isPlaying = false;
-
-// Function to toggle play/pause
-function togglePlayPause() {
-    if (isPlaying) {
-        video.pause();
-        playpause.innerHTML = '<i class="fa-solid fa-play"></i>';
-    } else {
-        videoThumbnail.style.display = "none";
-        video.play();
-        playpause.innerHTML = '<i class="fa-solid fa-pause"></i>';
-    }
-    isPlaying = !isPlaying;
+    return result;
 }
 
-document.addEventListener("keydown", function (event) {
-    if (event.key === 32 || event.key === " ") {
-        event.preventDefault();
+function reverseString(str) {
+    let result = ""
 
-        // Prevent scrolling the page down
-        togglePlayPause();
+    for (let i = str.length - 1; i >= 0; i--) {
+        result += str[i]
     }
-});
-
-// Event listener for the video to
-// update the isPlaying flag
-video.addEventListener("play", function () {
-    isPlaying = true;
-});
-
-video.addEventListener("pause", function () {
-    isPlaying = false;
-});
-
-video.addEventListener("ended", function () {
-    playpause.innerHTML = '<i class="fa-solid fa-play"></i>';
-});
-
-// Forward 5 sec or backward 5 sec
-frwd.addEventListener("click", function () {
-    video.currentTime += 5;
-});
-bkwrd.addEventListener("click", function () {
-    video.currentTime -= 5;
-});
-
-// Mute or Unmute
-mutebtn.addEventListener("click", function () {
-    if (video.muted) {
-        video.muted = false;
-        mutebtn.innerHTML = '<i class="fas fa-volume-up"></i>';
-        volume.value = video.volume;
-    } else {
-        video.muted = true;
-        mutebtn.innerHTML = '<i class="fa-solid fa-volume-xmark"></i>';
-        volume.value = 0;
-    }
-});
-
-document.addEventListener("keydown", function (event) {
-    if (event.key === "M" || event.key === "m") {
-        event.preventDefault();
-        if (video.muted) {
-            video.muted = false;
-            mutebtn.innerHTML = '<i class="fas fa-volume-up"></i>';
-            volume.value = video.volume;
-        } else {
-            video.muted = true;
-            mutebtn.innerHTML = '<i class="fa-solid fa-volume-xmark"></i>';
-            volume.value = 0;
-        }
-    }
-});
-
-volume.addEventListener("input", function () {
-    video.volume = volume.value;
-    if (video.volume === 0) {
-        mutebtn.innerHTML = '<i class="fa-solid fa-volume-xmark"></i>';
-    } else {
-        mutebtn.innerHTML = '<i class="fas fa-volume-up"></i>';
-    }
-});
-
-// Hide or unhide controllers div
-videoContainer.addEventListener("mouseenter", () => {
-    controls.style.opacity = 1;
-});
-
-videoContainer.addEventListener("mouseleave", () => {
-    controls.style.opacity = 0;
-});
-
-// Update the playback line as the video plays
-video.addEventListener("timeupdate", () => {
-    const currentTime = video.currentTime;
-    const duration = video.duration;
-    const percentage = (currentTime / duration) * 100;
-    progressBar.style.width = percentage + "%";
-});
-
-function showThumbnail() {
-    videoThumbnail.style.display = "block";
+    return result;
 }
 
-// Reseting the playback line when the video ends
-video.addEventListener("ended", () => {
-    progressBar.style.width = "0%";
-    showThumbnail();
-});
+function reverseNumber(num) {
+    let result = 0;
+    let rem = 0;
 
-setInterval(() => {
-    currentTimeRef.innerHTML = timeFormatter(video.currentTime);
-    maxDuration.innerText = timeFormatter(video.duration);
-}, 1);
+    while (num > 0) {
+        rem = num % 10;
+        result = rem + (result * 10); // 4
+        num = Math.floor(num / 10);
+    }
+    return result;
+}
 
-playbackline.addEventListener("click", (e) => {
-    let timelineWidth = playbackline.clientWidth;
-    video.currentTime = (e.offsetX / timelineWidth) * video.duration;
-});
+console.log(reverseNumber(1234))
+console.log(reverseString("eninracconsultingpvtltd"))
+console.log(pow(5, 4));
